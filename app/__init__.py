b99cb2a232470a4d6fac1ebcacd74589bd6bd60e
michael-muga <mugamichaelshawn@gmail.com>
2022-03-15 10:19:36 +0300

    registering blueprints and configuration of app

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+from ensurepip import bootstrap
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+def create_app(config_name):
+    
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    from .main import main as main_blueprint
+    from .auth import auth as auth_blueprint
+
+
+    #registering bluprint
+    app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint,url_prefix = '/auth')
+
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+
+
+    return app

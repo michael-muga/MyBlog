28dff972c8a89d4d754fc94ac11f3bfb36494aba
michael-muga <mugamichaelshawn@gmail.com>
2022-03-15 12:28:52 +0300

    mail and login manager extensions added

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,22 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
+
+
 
 def create_app(config_name):
     
@@ -22,10 +34,17 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
     app.register_blueprint(auth_blueprint,url_prefix = '/auth')
 
+    # configure UploadSet
+    configure_uploads(app,photos)
+
 
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+
 
 
 

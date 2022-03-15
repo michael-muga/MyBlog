4777f6c7a76070701b95a210b29bc0ecab037b89
michael-muga <mugamichaelshawn@gmail.com>
2022-03-15 12:37:55 +0300

    email configurations added

--- config.py
+++ config.py
@@ -6,6 +6,15 @@ class Config:
     General configuration parent class
     '''
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://michael:mike2020@localhost/myblog'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")    
+
 
 
     pass
